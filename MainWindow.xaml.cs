73ebbae15dbbd55e7c7dc91fbf1d80612ce201c2
Ray Salgado <rsalgado@pulte.com>
2020-01-08 11:16:41 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using rdrtwocontentmanager.Views;
+using System.Windows;
+
+namespace rdrtwocontentmanager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BModTargetLauncher_Click(object sender, RoutedEventArgs e)
+        {
+            ccMainContentContainer.Content = new ModTarget(ccMainContentContainer);
+        }
+    }
+}
---
+++ Views/ModTarget.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Controls;
+
+namespace rdrtwocontentmanager.Views
+{
+    /// <summary>
+    /// Interaction logic for ModTarget.xaml
+    /// </summary>
+    public partial class ModTarget : UserControl
+    {
+        public ContentControl ParentContainer { get; set; }
+        public ModTarget(object parentContainer)
+        {
+            InitializeComponent();
+
+            ParentContainer = (parentContainer as ContentControl);
+        }
+
+        private void bExit_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ParentContainer.Content = null;
+        }
+    }
+}
