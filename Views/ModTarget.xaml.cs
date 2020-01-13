ec1e6eee454d4e4f0f085fc58066f462c4e8b04d
Ray Salgado <rsalgado@pulte.com>
2020-01-13 10:46:23 -0500

    latest changes

--- Views/ModTarget.xaml.cs
+++ Views/ModTarget.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Forms;
 
 namespace rdrtwocontentmanager.Views
 {
     /// <summary>
     /// Interaction logic for ModTarget.xaml
     /// </summary>
-    public partial class ModTarget : UserControl
+    public partial class ModTarget : System.Windows.Controls.UserControl
     {
         public ContentControl ParentContainer { get; set; }
         public ModTarget(object parentContainer)
@@ -18,6 +19,17 @@ namespace rdrtwocontentmanager.Views
         private void bExit_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             ParentContainer.Content = null;
+        }
+
+        private void bFileSelect_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    tbFileLocation.Text = fbd.SelectedPath;
+                }
+            }
         }
     }
 }
